5c11fe3a99153395173e82a0bfb5d2245e70541c
Redoy Dash <hridoydash175@gmail.com>
2024-11-18 01:36:32 +0600

    color modifyed

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+// Script for FAQ
+document.addEventListener("DOMContentLoaded", function () {
     const questions = document.querySelectorAll(".question");
 
     questions.forEach(question => {
-        question.addEventListener("click", function() {
-            // Toggle the visibility of the answer
+        question.addEventListener("click", function () {
             const answer = this.nextElementSibling;
             answer.style.display = answer.style.display === "block" ? "none" : "block";
 
-            // Optionally, you can add an active class to the question to change its appearance
             this.classList.toggle("active");
         });
     });
 });
+
+
